25842d669fbc2bca98441b0c53f43ffb49ca9905
Stan <solve153@gmail.com>
2017-07-21 13:15:16 +0800

    use ajax select tag implement category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,13 @@ class PostsController < ApplicationController
     @post.save
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update!( post_params )
+
+    render :json => { :id => @post.id, :message => "ok"}
+  end
+
   def destroy
     @post = current_user.posts.find(params[:id])
     @post.destroy
@@ -59,6 +66,6 @@ class PostsController < ApplicationController
   protected
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :category_id)
   end
 end
